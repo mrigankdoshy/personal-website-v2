66b225b06a4aad3d50887d7e44ccf89aeb64ec4b
Mrigank Doshy <mrigankdoshy@gmail.com>
2025-01-19 13:02:24 -0500

    chore: update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  siteUrl: 'https://mrigankdoshy.com',
+  siteUrl: 'https://www.mrigankdoshy.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
