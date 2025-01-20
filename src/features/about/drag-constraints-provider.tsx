764ef987b2983471368bae0b8c72e48c71ef2598
Mrigank Doshy <mrigankdoshy@gmail.com>
2025-01-19 19:00:40 -0500

    fix: layout grow

--- src/features/about/drag-constraints-provider.tsx
+++ src/features/about/drag-constraints-provider.tsx
@@ -19,10 +19,7 @@ export function DragConstraintsProvider({
     <DragConstraintsContext.Provider
       value={constraintsRef as RefObject<HTMLDivElement>}
     >
-      <div
-        ref={constraintsRef}
-        style={{ position: 'relative', width: '100%', height: '100%' }}
-      >
+      <div ref={constraintsRef} className="flex flex-grow flex-col">
         {children}
       </div>
     </DragConstraintsContext.Provider>
