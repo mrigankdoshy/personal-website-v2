09f0e59f64f076e7871f839c69ef60f66aa58d2f
Mrigank Doshy <mrigankdoshy@gmail.com>
2025-01-31 21:29:51 -0500

    style: increase width

--- src/features/spotify/player-wrapper.tsx
+++ src/features/spotify/player-wrapper.tsx
@@ -9,7 +9,7 @@ export function PlayerWrapper({ children }: PlayerWrapperProps) {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex w-full max-w-md flex-col gap-8 rounded-xl bg-secondary/30 p-6 text-foreground shadow-md backdrop-blur-md"
+      className="flex w-full max-w-lg flex-col gap-8 rounded-xl bg-secondary/30 p-6 text-foreground shadow-md backdrop-blur-md"
     >
       {children}
     </motion.div>
