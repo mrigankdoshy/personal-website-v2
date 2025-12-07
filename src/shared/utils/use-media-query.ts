4ba0ee2f053b2d0eb13f8740e78e5b7d55170c3e
Mrigank Doshy <mrigankdoshy@gmail.com>
2025-12-06 19:55:44 -0500

    fix: media query hook

--- src/shared/utils/use-media-query.ts
+++ src/shared/utils/use-media-query.ts
@@ -1,15 +1,24 @@
 import { useCallback, useEffect, useState } from 'react';
 
 export function useMediaQuery(width: number): boolean {
-  const media = window.matchMedia(`(max-width: ${width}px)`);
+  const media =
+    typeof window !== 'undefined'
+      ? window.matchMedia(`(max-width: ${width}px)`)
+      : null;
 
-  const [targetReached, setTargetReached] = useState<boolean>(media.matches);
+  const [targetReached, setTargetReached] = useState<boolean>(
+    media?.matches || false
+  );
 
   const updateTarget = useCallback((e: MediaQueryListEvent) => {
     setTargetReached(e.matches);
   }, []);
 
   useEffect(() => {
+    if (!media) {
+      return;
+    }
+
     media.addEventListener('change', updateTarget);
 
     return () => {
